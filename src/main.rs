b376327500f85807961cbca73afd3a4ae13a37ce
Jakub Kicinski <kuba@kernel.org>
2026-09-12 13:51:02 -0700

    main: sort the progress stage list with sort_by_key
    clippy's unnecessary_sort_by fires on the manual reverse comparator.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- src/main.rs
+++ src/main.rs
@@ -1071,7 +1071,7 @@ fn render_progress(state: &mut ProgressState) {
                             (st, turn)
                         })
                         .collect();
-                    stages_with_turns.sort_by(|a, b| b.1.cmp(&a.1));
+                    stages_with_turns.sort_by_key(|item| std::cmp::Reverse(item.1));
 
                     let (top_stage, top_turn) = stages_with_turns[0];
                     let stage_name = stage_short_name(top_stage);
